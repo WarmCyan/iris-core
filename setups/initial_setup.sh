0e5c10305d0d847de5ff7b92408a55e117e0dc1e
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-12 14:16:11 -0600

    Initial commit

---
+++ setups/initial_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# =========================================
+# IRIS initial setup script
+# Date Created: 2019.12.12
+# Date Edited: 2019.12.12
+# Copyright (c) 2019 Digital Warrior Labs
+# Author: Nathan Martindale
+# Description: Minimum system base infrastructure setup
+# =========================================
+
+. integrity_check.sh
