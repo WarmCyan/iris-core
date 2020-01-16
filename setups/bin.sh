f8b505ffbfe63f3385531a14acacee6fd668591e
Nathan Martindale <nathanamartindale@gmail.com>
2020-01-16 15:56:31 -0600

    Clean up various issues with iris-edit

--- setups/bin.sh
+++ setups/bin.sh
@@ -4,5 +4,26 @@
 
 script_loc=$(dirname $0)
 
+setup_list="$DATA_DIR/iris/bin_setup_registry"
+
+# if there was previous file listing installed files, remove all of them
+if [ -f $setup_list ]; then
+	echo "Cleaning all files on previous bin registry..."
+	
+	pushd $BIN_DIR > /dev/null
+		rm -rfd $(cat $setup_list) > /dev/null 2>&1
+		# for file in $(cat $setup_list)
+		# do
+		# 	rm -rfd $file > /dev/null 2>&1
+		# done
+	popd > /dev/null # $BIN_DIR
+fi
+
 echo "Installing IRIS management scripts..."
 cp -r $script_loc/../iris/* $BIN_DIR
+
+# Save the new list of installed files to data
+echo "Saving new bin registry..."
+pushd $script_loc/../iris/ > /dev/null
+	ls > $setup_list
+popd > /dev/null # $script_loc/../iris/ 
